e53dbeceb765dec0b7818dcc230b47ac8d2ea168
Softode <softode-code@users.noreply.github.com>
2020-05-09 02:08:53 +0500

    scheduled the task

--- change_desktop_background.py
+++ change_desktop_background.py
@@ -1,12 +1,23 @@
 import os
 import random
 import ctypes
+import schedule
+import time
 
 Path = "E:\\Wallpapers\\"
 SPI_SETDESKWALLPAPER = 20
 
-
-def changeBG(path):
+def changeBG():
+    path = Path+random.choice(os.listdir(Path))
     ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER,0,path,3)
 
-changeBG(Path+random.choice(os.listdir(Path)))
+schedule.every().hours.do(changeBG)
+
+f = open('pid.txt','w+')
+pid = os.getpid()
+f.write(str(pid))
+f.close()
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
